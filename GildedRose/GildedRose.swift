186d10dee90ce4b4aaab4b52a47815eae8ebb2c7
Ceren Gazioglu Majoor <cgmajoor@gmail.com>
2019-12-04 17:43:25 +0100

    Extracted method updateQualityOf to act on a single item. All tests pass.

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -14,60 +14,65 @@ public class GildedRose {
         self.items = items
     }
     
-    public func updateQuality() {
+    fileprivate func updateQualityOf(_ item: Item) {
         
-        for i in 0..<items.count {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        if (items[i].name != "Conjured Mana Cake") {
-                            items[i].quality = items[i].quality - 1
-                        } else {
-                            items[i].quality = items[i].quality - 2
-                        }
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-                    
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                        
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-            
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
+        if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.quality > 0) {
+                if (item.name != "Sulfuras, Hand of Ragnaros") {
+                    if (item.name != "Conjured Mana Cake") {
+                        item.quality = item.quality - 1
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = item.quality - 2
+                    }
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+                
+                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
+                    }
+                    
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
+                    }
+                }
+            }
+        }
+        
+        if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.sellIn = item.sellIn - 1
+        }
+        
+        if (item.sellIn < 0) {
+            if (item.name != "Aged Brie") {
+                if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.quality > 0) {
+                        if (item.name != "Sulfuras, Hand of Ragnaros") {
+                            item.quality = item.quality - 1
+                        }
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    item.quality = item.quality - item.quality
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
                 }
             }
+        }
+    }
+    
+    public func updateQuality() {
+        
+        for item in items {
+            updateQualityOf(item)
         }
     }
 }
