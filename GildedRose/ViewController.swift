318061aa5a86627a673e0b971da9bc4c58f318fc
Ceren Gazioglu Majoor <cgmajoor@gmail.com>
2019-12-04 12:26:53 +0100

    Initial Commit

---
+++ GildedRose/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GildedRose
+//
+//  Created by Ceren Gazioglu Majoor on 04/12/2019.
+//  Copyright © 2019 Lucium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
